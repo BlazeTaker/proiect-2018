37973a40cf2fba91e47513750769c79bb4ecd77d
Ursu Razvan <razvan.ursu@ymail.com>
2017-12-25 10:23:57 +0200

    Made a demo interaface

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,4 +2,5 @@ Heyo, this is just here to let you know:
 
 - I'm using this for a personal project
 - It's gonna be filled with a lot of trial and error
-- I guess I had to put something in.
+- I need to start filling up this window with stuff.
+- Maybe an input window.
--- interfaceProject.java
+++ interfaceProject.java
@@ -1,9 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class interfaceProject {
 
 	public static void main(String[] args) {
-		System.out.println("First steps...");
-
+		JFrame projectInterface = new JFrame ("Interface");
+		projectInterface.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel emptyLabel = new JLabel("");
+		emptyLabel.setPreferredSize(new Dimension(175, 100));
+		projectInterface.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+		
+		projectInterface.pack();
+		projectInterface.setVisible(true);
 	}
 
 }
