4ae07005ff909dd05f6587a8ffc0149c3ea80220
Ursu Razvan <razvan.ursu@ymail.com>
2017-12-27 18:37:18 +0200

    Playing with frames a bit

--- interfaceProject.java
+++ interfaceProject.java
@@ -1,19 +1,42 @@
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.*;
 
 public class interfaceProject {
-
+	
+	private JFrame frame;
+	private JPanel panel;
+	private JButton buttonOne;
+	private JLabel label;
+	
+	public interfaceProject() {
+		
+		gui();
+		
+	}
+	
+	public void gui() {
+		
+		frame = new JFrame ("Interface Project");
+		frame.setVisible(true);
+		frame.setSize(600,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		panel = new JPanel();
+		panel.setBackground(Color.CYAN);
+		
+		buttonOne = new JButton("Press Me Gently");
+		label = new JLabel("Look at me");
+		
+		panel.add(buttonOne);
+		panel.add(label);
+		
+		frame.add(panel,BorderLayout.SOUTH);
+	}
+	
 	public static void main(String[] args) {
-		JFrame projectInterface = new JFrame ("Interface");
-		projectInterface.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel emptyLabel = new JLabel("");
-		emptyLabel.setPreferredSize(new Dimension(175, 100));
-		projectInterface.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+		new interfaceProject();
 		
-		projectInterface.pack();
-		projectInterface.setVisible(true);
 	}
 
 }
