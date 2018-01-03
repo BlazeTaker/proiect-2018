90ec1489799c727a18caa06b20d5572625a440e6
Ursu Razvan <razvan.ursu@ymail.com>
2018-01-03 20:25:09 +0200

    Playing some more with menu and image resizing

--- interfaceProject.java
+++ interfaceProject.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
+import java.awt.Image;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.io.*;
+
 
 public class interfaceProject {
 	
@@ -12,12 +17,27 @@ public class interfaceProject {
 	public void frame() {
 		
 		JFrame frame = new JFrame();
-		frame.setVisible(true);
 		frame.setSize(800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
 		JButton buttonOne = new JButton("Press me Gently");
+		
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File("D:\\\\Net\\\\Desktop Pics\\\\Goku.jpg"));
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+		JLabel label = new JLabel();
+		label.setSize(300, 150);
+		
+		Image dimg = img.getScaledInstance(label.getWidth(), label.getHeight(),
+		        Image.SCALE_SMOOTH);
+		
+		
+		label.setIcon(new ImageIcon(dimg));
 		
 		buttonOne.addActionListener(new ActionListener() {
 			
@@ -28,8 +48,21 @@ public class interfaceProject {
 			
 		});
 		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu file = new JMenu("File");
+		menu.add(file);
+		
+		frame.setJMenuBar(menu);
+		
+		JTextField textField = new JTextField(20);
+		textField.setToolTipText("Enter something");
+		
+		panel.add(label);
+		panel.add(textField);
 		panel.add(buttonOne);
 		frame.add(panel);
+		frame.setVisible(true);
 		
 	}
 	
